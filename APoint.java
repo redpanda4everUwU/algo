466bd4985de91a4de3c9fbe2b766654ca55523d4
Elailol <elenore.goudet@gmail.com>
2022-03-17 09:54:15 +0100

    add APoint

---
+++ APoint.java
@@ -0,0 +1,28 @@
+public class APoint {
+    // Attributs
+        
+    public double x;
+    public double y;
+        
+    // Constructeur
+       
+    public APoint(double ax, double ay){
+        x = ax;
+        y = ay;
+    }
+        
+    // Méthodes  
+             
+    public double distance( APoint otherPoint ) {
+        double dx = x - otherPoint.x;
+        double dy = y - otherPoint.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+        
+    public String toString() {
+        String res ="";
+        res="[x=" + x + ",y=" + y + "]";
+        return res;  
+    }    
+    
+}
