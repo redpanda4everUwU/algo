d6c54cf6163e9ed80ffb574c99059bb057a19a4d
redpanda4everUwU <pierre.noyer@insa-lyon.fr>
2022-03-31 09:59:16 +0200

    trajectoire

---
+++ Trajectoire.java
@@ -0,0 +1,28 @@
+public class Trajectoire {
+
+    APoint P; 
+    Ballon Bal;
+    //Motion M0; la vitesse initiale et l'angle 
+
+
+public Trajectoire(Ballon b){
+        Bal=b;
+        //  P=Bal.getPostion();
+        // M0=Bal.getMotion();
+        // recalcul d'angle avec variation de x et y sur la postion et la postion précedente ? 
+} 
+
+
+
+//get données 
+
+//équation
+
+
+
+//équations rebond (get theta,vx,vy)
+
+/*afficher une preview de la trajectoire ?
+afficher le motion du ballon 
+faire une autre classe pour le déplacement ?*/ 
+}
