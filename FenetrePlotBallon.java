3eb249b28a2d905818e461ba67480b2dd1c217f2
Elailol <elenore.goudet@gmail.com>
2022-04-03 14:01:42 +0200

    implémentation de mouseListener et mouseMotionListener

--- FenetrePlotBallon.java
+++ FenetrePlotBallon.java
@@ -2,7 +2,7 @@ import javax.swing.* ;
 import java.awt.*;
 import java.awt.event.* ;
 
-public class FenetrePlotBallon extends JFrame implements ActionListener{
+public class FenetrePlotBallon extends JFrame implements ActionListener, MouseListener, MouseMotionListener{
 	
     // Les attributs
 	private Ballon monBallon ;
@@ -71,6 +71,55 @@ public class FenetrePlotBallon extends JFrame implements ActionListener{
         if (monBallon!=null)
             monBallon.deplaceY(10);
         repaint();
+    }
+
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 	
 }	
